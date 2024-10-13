0b69f4bd2b0cf9b31180bbe81ee34a1ccaa23e99
Idrees-28 <idreesjee2810@gmail.com>
2024-10-13 15:12:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar"
+import Navbar2 from "./components/Navbar2"
+import Saleimg from "./components/Saleimg"
+import Topics from "./components/Topics"
+import Footer from "./components/Footer"
+import Couse from "./components/Course"
+import Popular from "./components/Popular"
+
+function App(){
+    return(
+        <> <Navbar></Navbar>
+        <Navbar2></Navbar2>
+        <Saleimg></Saleimg>
+        <Couse></Couse>
+        <Topics></Topics>
+        <Popular></Popular>
+        <Footer></Footer> 
+                         
+</>
+    )
+}
+ export default App
